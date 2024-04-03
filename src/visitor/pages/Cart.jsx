c7539d726c8d9109813eaae80877ee55122b5362
walid1508 <2703593@collegelacite.ca>
2024-04-03 00:26:38 -0400

    fix CategoryNav routes

---
+++ src/visitor/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Helmet} from "react-helmet";
+
+const Cart = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Audiophile | Cart</title>
+                <meta name="description" content="Headphones page" />
+            </Helmet>
+            Hello from Cart
+        </div>
+    );
+};
+
+export default Cart;
